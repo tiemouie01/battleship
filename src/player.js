52ce70c45b6cd0334db09503d5d23c77b02c6089
Timothy Miamba <timal0361@gmail.com>
2024-01-16 19:05:59 +0200

    Add computer logic that randomly adds ships to the board

--- src/player.js
+++ src/player.js
@@ -1,7 +1,42 @@
 import Gameboard from "./gameboard";
 
+const generateDirection = function generateRandomDirectionValue() {
+  return Math.floor(Math.random() * 2) === 0 ? "x" : "y";
+};
+
+const addShipRandomly = function addSpecificShipToGameboardRandomly(board, name) {
+  // Add ships separately but randomise their coordinates using the board.
+  const gameboard = board.getBoard();
+  let index = Math.floor(Math.random() * 100);
+  let position = gameboard[index];
+  let direction = generateDirection();
+
+  // Use a try-catch block to insert the named ship onto the board.
+  let successful = false;
+
+  while (!successful) {
+    try {
+      board.addShip(name, position, direction);
+      successful = true;
+    } catch {
+      index = Math.floor(Math.random() * 100);
+      position = gameboard[index];
+      direction = generateDirection();
+    }
+  }
+};
+
 export default function Player(name = null) {
   const board = Gameboard();
+
+  // If the name of a player is not specified. Randomly assign ships onto the board.
+  if (!name) {
+    addShipRandomly(board, "Carrier");
+    addShipRandomly(board, "Battleship");
+    addShipRandomly(board, "Destroyer");
+    addShipRandomly(board, "Submarine");
+    addShipRandomly(board, "Patrol Boat");
+  }
 
   const getName = () => {
     if (!name) return "Computer";
