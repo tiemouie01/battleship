5243a5886434648c958483c0dd546418c627a707
Timothy Miamba <timal0361@gmail.com>
2024-01-29 08:31:22 +0200

    Change entry to game.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/script.js",
+  entry: "./src/game.js",
   devtool: "source-map",
   devServer: {
     static: "./dist",
